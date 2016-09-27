896a685afb2f3c331e730f63a65e40d7410eedb1
Dylan Piercey <pierceydylan@gmail.com>
2016-09-27 14:32:42 -0700

    Fix error name

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var STATUS_CODES = require('@rill/http').STATUS_CODES
 
 module.exports = HttpError
 
-// Todo make external lib.
 function HttpError (code, message, meta) {
+  this.name = 'HttpError'
   this.code = code
   this.message = message || STATUS_CODES[code]
-  Error.call(this)
-  if (Error.captureStackTrace) Error.captureStackTrace(this, HttpError)
   for (var key in meta) this[key] = meta[key]
+  if (Error.captureStackTrace) Error.captureStackTrace(this, HttpError)
+  else Error.call(this)
 }
 
-HttpError.prototype = Object.create(Error)
+HttpError.prototype = new Error
